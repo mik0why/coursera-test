a572465442b80b39049640104afbb9c1dc521a86
mik0why <mikowhy72@gmail.com>
2020-05-14 12:33:08 -0400

    update app and index, simple name value calculator

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-(function(){
+(function () {
+'use strict';
+
+angular.module('NameCalculator', [])
+
+.controller('NameCalculatorController', function($scope){
+  $scope.name = "";
+  $scope.totalValue = 0;
 
 
+  $scope.displayNumeric = function(){
+    var value = calculateNumericString($scope.name);
+    $scope.totalValue = value; // update the scope val once calculated
+  };
+
+
+
+  function calculateNumericString(string){
+    var totalStringValue = 0;
+    for(var i = 0; i < string.length; i++){
+      totalStringValue += string.charCodeAt(i)
+    }
+    return totalStringValue;
+  }
+
+//everything within the scope
+});
 
 })();
